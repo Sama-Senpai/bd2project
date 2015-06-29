a84931b2bec4a795549efee22518f76043cad822
Jose <Jose@PC-Less>
2015-06-29 10:19:13 +0900

    aaa

--- bdconsultas.sql
+++ bdconsultas.sql
@@ -1,5 +1,9 @@
+
+------------------------------------------------------------------------------------------------------------------------------------------
+ Este procedimiento agrega las habitaciones por hoteles y sus respectivas habitaciones, ademas edita el valor de la variable en hotel n_hab
+
 DELIMITER //
-CREATE PROCEDURE add_habxhotel (hotel_cod INT(11), cat_hab INT(11), precio int (11), cant int(11), n_adi int(11), descrp varchar(30))
+CREATE PROCEDURE add_habxhotel (hotel_cod INT(11), cat_hab INT(11), precio int (11), cant int(11), n_adi int(11), descrp varchar(150))
 BEGIN 
 
 DECLARE v1 INT DEFAULT 0;
@@ -29,8 +33,27 @@ VALUES (hotel_cod, cat_hab, precio, cant, n_adi, descrp);
 END //
 DELIMITER ;
 
+
+
+
+
+------------------------------------------------------------------------------------------------------------------------------------------
 //Aqui esta mmgvo
-SELECT reserva_hospedaje.cod_hotel,reserva_hospedaje.cod_CH,COUNT(reserva_hospedaje.cod_CH) AS ocupadas,habitacion_hotel.cantidad,habitacion_hotel.descripcion,habitacion_hotel.precio_dia,habitacion_hotel.NroAdicionales
-FROM reserva_hospedaje INNER JOIN habitacion_hotel ON reserva_hospedaje.cod_hotel=habitacion_hotel.Hotel_cod AND reserva_hospedaje.cod_CH=habitacion_hotel.catalogo_habitacion_cod_ch
-WHERE reserva_hospedaje.cod_hotel="2" AND reserva_hospedaje.rc!="3" AND reserva_hospedaje.f_inicio<"2015-06-08" AND reserva_hospedaje.f_fin>"2015-05-25"
+
+DELIMITER //
+CREATE PROCEDURE sel_rooms (hotel_cod INT(11), fh_ini DATE, fh_fin DATE)
+BEGIN 
+
+SELECT reserva_hospedaje.cod_hotel, reserva_hospedaje.cod_CH,COUNT(reserva_hospedaje.cod_CH) AS ocupadas, 
+       habitacion_hotel.cantidad, habitacion_hotel.descripcion, habitacion_hotel.precio_dia, habitacion_hotel.NroAdicionales
+FROM reserva_hospedaje INNER JOIN habitacion_hotel ON reserva_hospedaje.cod_hotel=habitacion_hotel.Hotel_cod AND 
+                                                      reserva_hospedaje.cod_CH=habitacion_hotel.catalogo_habitacion_cod_ch
+WHERE reserva_hospedaje.cod_hotel=hotel_cod AND 
+      reserva_hospedaje.rc!="3" AND 
+      reserva_hospedaje.f_inicio<fh_fin AND 
+      reserva_hospedaje.f_fin>fh_ini
 GROUP BY reserva_hospedaje.cod_CH;
+
+END //
+DELIMITER ;
+
