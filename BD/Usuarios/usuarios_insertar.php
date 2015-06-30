54779b4a73b8619cdf0aba9082acb5ecfc6fdee4
nico722 <nicolas.cardenas722@gmail.com>
2015-06-29 20:29:39 -0430

    Update usuarios_insertar.php

--- BD/Usuarios/usuarios_insertar.php
+++ BD/Usuarios/usuarios_insertar.php
@@ -4,18 +4,15 @@
         include '../conexion.php';
 
 	// Recibe las Variables
-	$firstname	= $_POST['firstname'];
-	$lastname	= $_POST['lastname'];
-        $cedula         = $_POST['cedula'];
-        $emailaddress	= $_POST['emailaddress'];
-        $password	= $_POST['password'];
-        $password2	= $_POST['password2'];
-        $sex    	= $_POST['sex'];
-        $nacimiento	= $_POST['nacimiento'];
-        $country	= $_POST['country'];
-        $city   	= $_POST['city'];
-        $telefono	= $_POST['telefono'];
-        $direccion	= $_POST['direccion'];
+	$cedula         = filter_input(INPUT_POST, 'cedula');
+        $firstname	= filter_input(INPUT_POST, 'firstname');
+	$telefono	= filter_input(INPUT_POST, 'telefono');
+        $direccion	= filter_input(INPUT_POST, 'direccion');
+        $emailaddress	= filter_input(INPUT_POST, 'emailaddress');
+        $nacimiento	= filter_input(INPUT_POST, 'nacimiento');
+        
+        
+        
         
         date_default_timezone_set ("America/Caracas");
         
@@ -40,8 +37,8 @@
         $edad=($ano-$anioU);
         
         // Verifica si la Ci ingresada existe en la base de datos
-        $queryCI = mysql_num_rows(mysql_query("SELECT * FROM Usuarios WHERE Ci='$cedula'"));
-        $queryEmail = mysql_num_rows(mysql_query("SELECT * FROM Usuarios WHERE Correo='$emailaddress'"));
+        $queryCI = mysql_num_rows(mysql_query("SELECT * FROM cliente WHERE Ci='$cedula'"));
+        $queryEmail = mysql_num_rows(mysql_query("SELECT * FROM cliente WHERE Correo='$emailaddress'"));
         
         if(($queryCI > 0) || ($queryEmail > 0)){    // Si la Ci o el Correo existe
             if($queryCI > 0){
@@ -52,56 +49,27 @@
             }
         }else{      // Si la Ci no existe
             
-            if ($edad<18){     // Si es menor a 18 
-                header("Location:../../registro.php?errorCode=7&errorType=1");
-            }else{      // Si es mayor a 18
+           
 
-                if (!$firstname || !$lastname || !$cedula || !$emailaddress || !$password || !$password2 || !$sex || !$nacimiento || !$country || !$city || !$telefono || !$direccion){
+                if (!$firstname || !$cedula || !$emailaddress || !$nacimiento || !$telefono || !$direccion){
                     header("Location:../../registro.php?errorCode=5&errorType=4");
                 }else{      // Si todos los campos estan llenos
-                    if($password == $password2){   
+                       
 
                         // SQL
-                        $query = mysql_query("INSERT INTO Usuarios
+                        $query = mysql_query("INSERT INTO cliente
                                                     (
-                                                    Ci,Nombre,Apellido,Fecha_de_nacimiento,Telefono,Pais,Direccion,Sexo,Password,Correo,Ciudad
+                                                    Ci,nombre,n_tlf,direccion,dir_elec,f_nac
                                                     )
                                                     VALUES
                                                     (
-                                                    '$cedula','$firstname','$lastname','$nacimiento','$telefono','$country','$direccion','$sex','$password','$emailaddress','$city'
+                                                    '$cedula','$firstname','$telefono','$direccion','$emailaddress','$nacimiento'
                                                     )
                                             ");
-
-                        if(!$query){
-                            echo 'Error al insertar un usuario';
-                        }else
-                            echo 'Guardo Usuario Correctamente';
-
-                        $queryt =  mysql_query("INSERT INTO Transacciones 
-                                                    (
-                                                    Ci,Tipo_de_transaccion,Monto,Saldo,Fecha,Hora
-                                                    )
-                                                    VALUES
-                                                    (
-                                                    '$cedula','Iniciar','0','0','$fecha','$hora'
-                                                    )
-                                              ");
-
-                        if(!$queryt){
-                            echo 'Error al crear Transaccion';
-                        }else
-                            echo 'Guardo Transaccion Correctamente';
-                        if(isset($_GET['idMesa'])){
-                            // Redireccion
-                            header("Location:../../login.php?errorCode=5&errorType=3&idMesa=".$_GET['idMesa']);	
-                        }else{
-                            // Redireccion
-                            header("Location:../../login.php?errorCode=5&errorType=3");
-                        }    
-                    }else{
-                        header("Location:../../registro.php");
-                    }
+                 
+                        header("Location:../../registro_cliente.php");
+                    
                 }
-            }
+            
         }
 ?>
